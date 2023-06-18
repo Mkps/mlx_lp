03ceef0aa22efd67404dc548b3b108855708d9ff
Alexis LOUBIERE <alex.loubiere@gmail.com>
2023-06-18 20:13:38 +0200

    fixed memleaks

--- src/main.c
+++ src/main.c
@@ -3,40 +3,41 @@
 
 int	main(void)
 {
-	t_data	img;
-	void	*mlx;
-	void	*wnd;
+	// t_data	img;
+	// void	*mlx;
+	// void	*wnd;
 	int	WIDTH;
 	int	HEIGHT;
 	// int		color;
 	// int		r;
 	// int		b;
 	t_vars	vars;
+	t_data	img;
 	WIDTH = 1000;
 	HEIGHT = 1000;
-	mlx = mlx_init();
-	wnd = mlx_new_window(mlx, WIDTH, HEIGHT, "mlx");
-	img.img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	vars.mlx = mlx_init();
+	vars.window = mlx_new_window(vars.mlx, WIDTH, HEIGHT, "mlx");
+	img.img = mlx_new_image(vars.mlx, WIDTH, HEIGHT);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	vars.mlx = mlx;
-	vars.window = wnd;
 	vars.img = &img;
+	// vars.mlx = mlx;
+	// vars.window = wnd;
+	// vars.img = &img;
 	// r = 255;
 	// b = 255;
 	// color = create_argb(255, r, 0, b);
-	// draw_square(&img, 450, color);
+	// draw_rect(vars.img->img, 450, 450, 0xFFFF0000);
 	// draw_square(&current, 150, 0x00FF0000);
-	ft_mlx_fill(img, 40, 640, 0xFFFF0000);
+	// ft_mlx_fill(img, 40, 640, 0xFFFF0000);
 	// ft_draw_line(&img, 200, 200, 300, 300, color);
 	// ft_draw_line(&img, 100, 300, 200, 200, color);
 	// ft_draw_line_thick(&img, 100, 300, 300, 300, color, 6);
-	// draw_triangle(&img, 300, 300, 150, 0xFFFF0000);
-	// ft_mandelbrot(&img, WIDTH, HEIGHT);	
-	mlx_put_image_to_window(mlx, wnd, img.img, 0, 0);
-	// mlx_string_put(vars.mlx, vars.window, WIDTH / 3, HEIGHT / 20, 0xFFFFFFFF, "*** Mandelbrot ***");
-	mlx_hook(vars.window, 17, 1L<<4, ft_mlx_destroy_hook, &vars); 
+	// draw_triangle(vars.img->img, 300, 300, 150, 0xFFFF0000);
+	ft_mandelbrot(&img, WIDTH, HEIGHT);	
+	mlx_put_image_to_window(vars.mlx, vars.window, vars.img->img, 0, 0);
+	// // mlx_string_put(vars.mlx, vars.window, WIDTH / 3, HEIGHT / 20, 0xFFFFFFFF, "*** Mandelbrot ***");
+	mlx_hook(vars.window, 17, 0, ft_mlx_destroy_hook, &vars); 
 	mlx_hook(vars.window, 2, 1L<<0, ft_mlx_close_window, &vars);
-	mlx_loop(mlx);
-	free(mlx);
+	mlx_loop(vars.mlx);
 	return (0);
 }
--- src/window.c
+++ src/window.c
@@ -1,22 +1,27 @@
 #include "../includes/mlx_lp.h"
 
+void	ft_mlx_exit(t_vars *vars)
+{
+	mlx_destroy_image(vars->mlx, vars->img->img);
+	mlx_destroy_window(vars->mlx, vars->window);
+	mlx_destroy_display(vars->mlx);
+	free(vars->mlx);
+	exit(0);
+}
 int	ft_mlx_close_window(int keycode, t_vars *vars)
 {
 	ft_printf("keycode is %i\n", keycode);
 	if (keycode == 65307 || keycode == 27)
 	{
-		mlx_destroy_image(vars->mlx, vars->img->img);
-		mlx_destroy_window(vars->mlx, vars->window);
-		free(vars->mlx);
-		exit(0);
+		ft_mlx_exit(vars);
 	}
 	return (0);
 }
 
 int ft_mlx_destroy_hook(t_vars *vars)
 {
-	mlx_destroy_window(vars->mlx, vars->window);
-	exit(0);
+	ft_mlx_exit(vars);
+	return (0);
 }
 void	ft_mlx_fill(t_data img, int size_x, int size_y, int color)
 {
